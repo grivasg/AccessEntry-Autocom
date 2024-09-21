9a75d2cdfbc1ba26fb056151e1fbf9b14feaa577
Roger <ronguerra51@gmail.com>
2024-09-20 18:42:38 -0600

    ACTUALIZACION WEBPACK

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: {
     'js/app' : './src/js/app.js',
     'js/inicio' : './src/js/inicio.js',
+    'js/indigo' : './src/js/indigo.js',
   },
   output: {
     filename: '[name].js',
