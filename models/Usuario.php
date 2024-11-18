7b75dfeec68703a37ba79a987796c53a95507e23
SUBTENIENTE DE INFANTERIA FRANCISCO DANIEL RIVAS GARCIA <fdrivas@mindef.mil.gt>
2024-11-18 10:20:55 -0600

    creacion modelo para usuario

---
+++ models/Usuario.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Model;
+
+class Solicitud extends ActiveRecord
+{
+    protected static $tabla = 'usu_cred';
+    protected static $idTabla = 'usu_id';
+    protected static $columnasDB = ['usuario', 'password'];
+
+    public $usu_id;
+    public $usuario;
+    public $password;
+
+
+    public function __construct($args = [])
+    {
+        $this->usu_id = $args['usu_id'] ?? null;
+        $this->usuario = $args['usuario'] ?? '';
+        $this->password = $args['password'] ?? '';
+    }
+}
